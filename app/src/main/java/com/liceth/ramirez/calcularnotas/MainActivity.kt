80974b4bbe417119bdec012c58484ca39339f0fd
Licethramirez <licethr984@gmail.com>
2024-02-15 09:07:21 -0500

    Initial commit

---
+++ app/src/main/java/com/liceth/ramirez/calcularnotas/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.liceth.ramirez.calcularnotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var ingresarNombre : EditText
+    private lateinit var ingresarPorcentaje : EditText
+    private lateinit var ingresarNota: EditText
+    private lateinit var finalizar : Button
+    private lateinit var guardar : Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ingresarNombre = findViewById(R.id.ingresarNombre)
+        ingresarPorcentaje = findViewById(R.id.ingresarNombre)
+        ingresarNota = findViewById(R.id.ingresarNota)
+        finalizar = findViewById(R.id.finalizar)
+        guardar = findViewById(R.id.guardar)
+
+
+
+    }
+
+
+}
