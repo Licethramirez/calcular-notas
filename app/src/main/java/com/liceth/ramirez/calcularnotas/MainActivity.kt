4d8cc9ce00262898ef484ebb26aba543b80d1840
ADMIN <licethr984@gmail.com>
2024-03-07 10:05:13 -0500

    se declararon dos textView y se agrego un siguiente estudiante Button y se creo un finalizar set0nClic se inicializaron vistaPromedio, vistaNotaFinal y siguienteEstudiante se creo una funcion nuevoEstudiante se inicializo el botom de siguienteEstudiante se puso un return para que no tuviera tanto decimal el promedio se habilito la casilla de ingresarNombre se inhabilito el botom de finalizar se inhabilito el botom de siguienteEstudiante

--- app/src/main/java/com/liceth/ramirez/calcularnotas/MainActivity.kt
+++ app/src/main/java/com/liceth/ramirez/calcularnotas/MainActivity.kt
@@ -107,6 +107,8 @@ class MainActivity : AppCompatActivity() {
         vistaNotaFinal.text = ""
 
         ingresarNombre.isEnabled = true
+        finalizar.isEnabled = false
+        siguienteEstudiante.isEnabled = false
     }
     fun mostrarMensaje(mensaje : String){
         Toast.makeText(this,
